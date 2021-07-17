10354cc127d2b10b5c4e044e2f008adab4105eca
James Feldmayer <36408535+James-Feldmayer@users.noreply.github.com>
2021-07-17 19:46:24 +1000

    Create feldy.hpp

---
+++ namespace detail {     template <typename C>     struct to_helper {     };          template <typename Container, rng::range R>     requires std::convertible_to<rng::range_value_t<R>, typename Container::value_type>     Container operator|(R&& r, to_helper<Container>) {         return Container{r.begin(), r.end()};     } }  template <rng::range Container> requires (!rng::view<Container>) auto to() {     return detail::to_helper<Container>{}; }/feldy.hpp
@@ -0,0 +1,19 @@
+namespace detail {
+    template <typename C>
+    struct to_helper {
+    };
+    
+    template <typename Container, rng::range R>
+    requires std::convertible_to<rng::range_value_t<R>, typename Container::value_type>
+    Container operator|(R&& r, to_helper<Container>) {
+        return Container{r.begin(), r.end()};
+    }
+}
+
+template <rng::range Container>
+requires (!rng::view<Container>)
+auto to() {
+    return detail::to_helper<Container>{};
+}
+
+// | to<std::vector<int>>()
