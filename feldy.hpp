2af63203e1627d21fca0bfedf2f25256aa6e702b
James Feldmayer <36408535+James-Feldmayer@users.noreply.github.com>
2021-07-17 19:49:15 +1000

    Update feldy.hpp

--- feldy.hpp
+++ feldy.hpp
@@ -3,4 +3,22 @@ auto const print = [](auto const &...e) { (std::cout << ... << e); };
 
 auto const println = [](auto const &...e) { print(e..., '\n'); };
 
-//
+namespace detail {
+    template <typename C>
+    struct to_helper {
+    };
+    
+    template <typename Container, rng::range R>
+    requires std::convertible_to<rng::range_value_t<R>, typename Container::value_type>
+    Container operator|(R&& r, to_helper<Container>) {
+        return Container{r.begin(), r.end()};
+    }
+}
+
+template <rng::range Container>
+requires (!rng::view<Container>)
+auto to() {
+    return detail::to_helper<Container>{};
+}
+
+// | to<std::vector<int>>()
